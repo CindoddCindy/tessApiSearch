053542e87f1c9b1dd0dc451c85b97487289611c5
CindoddCindy <cindodcindy@gmail.com>
2021-01-24 15:10:21 +0700

    tambah package

---
+++ testsearchspring/src/main/java/springsearchtest/testsearchspring/TestsearchspringApplication.java
@@ -0,0 +1,13 @@
+package springsearchtest.testsearchspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsearchspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsearchspringApplication.class, args);
+	}
+
+}
---
+++ testsearchspring/src/test/java/springsearchtest/testsearchspring/TestsearchspringApplicationTests.java
@@ -0,0 +1,13 @@
+package springsearchtest.testsearchspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsearchspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
